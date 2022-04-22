541fff119e368879bf0b559215c65c7d162dd350
jasonwieczorek <wieczorek.jason@gmail.com>
2022-04-21 21:20:12 -0500

    added Function samples

---
+++ src/main/java/j/m/w/functionals/GameFunction.java
@@ -0,0 +1,27 @@
+package j.m.w.functionals;
+
+import j.m.w.games.Game;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * A simple class demonstrating the use of a BiFunction.
+ */
+public class GameFunction {
+
+    // arg1, arg2, return type of the Function
+    private static BiFunction<Game, Double, Game> discountGame = (game1, discount) -> {
+        game1.setCost(game1.getCost() * discount);
+        return game1;
+    };
+
+    public static Game discountGame(Game game, Double percent) throws IllegalArgumentException{
+
+       if (percent >= 1 || percent <= 0) {
+            throw new IllegalArgumentException("Discount must be a percentage");
+       }
+
+      return discountGame.apply(game, percent);
+    }
+}
